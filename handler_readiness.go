39d38b6c75fdd34dc3c4a0312e3084a8352ff6f3
Ajoy Dey <93241259+ajoydey00001@users.noreply.github.com>
2025-05-18 21:29:31 +0600

    Update handler_readiness.go

--- handler_readiness.go
+++ handler_readiness.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	
 	"net/http"
 )
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	// log.Println("Readiness probe")
-	respondWithJSON(w,200,struct{}{})
+	respondWithJSON(w, 200, struct{}{})
 }
+
+//test
